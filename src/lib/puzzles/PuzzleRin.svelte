09643b58d0ba3d13e9ae580cd13946f2e4f9b577
Suyooo <me@suyo.be>
2023-02-13 14:37:55 +0100

    Fix typo on Rin puzzle

--- src/lib/puzzles/PuzzleRin.svelte
+++ src/lib/puzzles/PuzzleRin.svelte
@@ -13,7 +13,7 @@
         Rin, and the "run" from the original was also in reference to her. The first line's word changed from a verb to
         a noun though - I wonder if that is more confusing to people who cleared the Japanese puzzle than those who see
         it for the first time?<br><br>
-        Doesn't matter much anyways, because I made sure to make the obvious answer is still obvious. Because, you know,
-        if you're supposed to answer Rin's Puzzle with a type of food, how many possible answers are there, really.
+        Doesn't matter much anyways, because I made sure that the obvious answer is still obvious. Because, you know, if
+        you're supposed to answer Rin's Puzzle with a type of food, how many possible answers are there, really.
     </div>
 </ModalPuzzleBase>
