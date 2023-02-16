1b597199e434de6ea7724273e4e504294e485d9c
Suyooo <me@suyo.be>
2023-02-16 23:51:59 +0100

    Chika puzzle

---
+++ src/lib/puzzles/PuzzleChika.svelte
@@ -0,0 +1,29 @@
+<script>
+    import ModalPuzzleBase from "$lib/ModalPuzzleBase.svelte";
+</script>
+
+<ModalPuzzleBase key="chika" solution="shine" url="/puzzles/11_chika.png">
+    <!--<div slot="hint">
+        The key to this puzzle is figuring out what "fill with" means. Try working backwards - you have two examples to
+        work with.<br>
+        What do the example words have in place of the circles? Where could these have come from?
+    </div>-->
+    <div slot="explain">
+        In this puzzle, "fill with" means to replace the circle symbols with the letters of the word "island". There are
+        three words possible, with "shade" and "shale" given as examples. The final possibility is the answer, "shine".
+    </div>
+    <div slot="loc">
+        It took me an entire day to find a emoji/answer combination where this puzzle was possible. I was close to just
+        dumping the original puzzle and making something different lol<br><br>
+        One thing I ruled out very early was using the mikan as in the original - simply because some people would call
+        it a "mikan", but others would go for something like "orange" or "tangerine", since it's a localized puzzle. In
+        these cases, it would be extremely confusing if you run into a problem and then can't tell whether it's your
+        approach or your citrus name. It would be impossible to make sure everyone is on the same page without heavy
+        descriptions or hinting, so I just skipped the good fruit for today.<br><br>
+        Also, this puzzle is once again something where in the original, the mere existence of kanji raises the
+        difficulty of the puzzle by obscuring the reading of the words. So, in turn, I had two spots to fill in instead
+        of one to make it a little harder here. The fact that "island" is twice as many characters as "mi-ka-n" also
+        makes things harder, but that one I tried to soften by giving two examples instead of just one, so you can see
+        more characters being used.
+    </div>
+</ModalPuzzleBase>
