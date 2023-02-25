c97eebab410f27c01b780a9bc1a3ef0f15a82f2e
Suyooo <me@suyo.be>
2023-02-25 20:55:59 +0100

    Some words on the trapezium pick

--- src/lib/puzzles/PuzzleHanamaru.svelte
+++ src/lib/puzzles/PuzzleHanamaru.svelte
@@ -25,6 +25,16 @@
         Two, it just lined up nicely. I wanted to do the same thing as in the original puzzle where the stacks are
         placed at the same height, as if on a shelf, so I had to make sure all the used letters are in the same position
         in each word, but from the end. So I just wrote a little script, fed it with some shape names to test it out,
-        and "zura" as a possible word was just too perfect to pass up on.
+        and "zura" as a possible word was just too perfect to pass up on.<br><br>
+        "Trapezium" might feel like it's a little out there as a shape name. At first, I wanted the "z" to be the given
+        letter, and have the "a" be the result of a "hexagon" stack, but I thought the answer would be too obvious if
+        you saw it's a four letter word, starting with "z", and it's related to Maru. That's why I had to turn the "z"
+        into a stack, and the trapezium was the only match here.<br>
+        However, it still works fine, because the same thing can be applied still, but in reverse: if you see the answer
+        ends in "ura", it is also very obvious what the answer is, so you can still solve the puzzle even if you don't
+        know the word "trapezium". The difference being that instead of being obvious from the start, you still have to
+        figure out the approach to the puzzle first.<br>
+        I'm also very glad the entire trapezium/trapezoid confusion doesn't matter as they are the same length and have
+        the "z" in the same place, otherwise I'd be in big trouble...
     </div>
 </ModalPuzzleBase>
