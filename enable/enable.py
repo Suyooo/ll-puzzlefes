43280c0d4f3f575f801e7e898323008f4609a981
Suyooo <me@suyo.be>
2023-02-25 18:13:58 +0100

    ENABLE helper for finding words with as many matches as possible

--- enable/enable.py
+++ enable/enable.py
@@ -45,3 +45,24 @@ def combinations(target):
                             allHit = False
                             break
                     if maxHits > 0 and allHit: print(w1,o1,w2,o2)
+
+def sortByNonMatches(matchingLetters, mustInclude="", minMatching=0, maxNonMatching=9999):
+    r = {}
+    for w in l:
+        hasIncludes = [False for c in mustInclude]
+        countMatches = 0
+        countNonMatches = 0
+        for c in w:
+            if c in mustInclude:
+                hasIncludes[mustInclude.index(c)] = True
+            if c in matchingLetters:
+                countMatches += 1
+            else:
+                countNonMatches += 1
+
+        if False in hasIncludes: continue
+        if countMatches < minMatching: continue
+        if countNonMatches > maxNonMatching: continue
+        if countNonMatches not in r: r[countNonMatches] = []
+        r[countNonMatches].append(w)
+    return {k:r[k] for k in sorted(r)}
