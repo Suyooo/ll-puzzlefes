6e76eb647aaea0d06cbc4eb35017204ed1efea6f
Suyooo <me@suyo.be>
2023-02-25 19:48:14 +0100

    Mari puzzle (free localization =D)

---
+++ src/lib/puzzles/PuzzleMari.svelte
@@ -0,0 +1,19 @@
+<script>
+    import ModalPuzzleBase from "$lib/ModalPuzzleBase.svelte";
+</script>
+
+<ModalPuzzleBase key="mari" solution="shiny" url="/puzzles/18_mari.png">
+    <!--<div slot="hint">
+        Try describing how the two characters are placed relative to each other.<br>
+        You probably know what word the icon in the first line is supposed to represent. Try writing it out, and see
+        whether anything catches your eye.
+    </div>-->
+    <div slot="explain">
+        The first line has a "w" inside a "k". "w in k" - resulting in "wink".<br>
+        By the same logic, a "y" inside a "sh" gives you the answer "shiny".
+    </div>
+    <div slot="loc">
+        No notes, this is literally just the original puzzle copypasted :^) I didn't do anything except slap on the
+        usual translated template bits
+    </div>
+</ModalPuzzleBase>
