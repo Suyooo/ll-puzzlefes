aa1e36217572867d0749ec95de2d821fd0298b42
Suyooo <me@suyo.be>
2023-03-08 14:22:20 +0100

    Create the mapping for the final puzzle

--- puzzles/final-test.py
+++ puzzles/final-test.py
@@ -3,10 +3,10 @@ answers = [
     "bakery", "angelic", "sewing", "training", "ramen", "bibi", "muse", "rice", "sister",
     "clover", "piano", "dive", "fan", "ferry", "circle", "zura", "shiny", "yarn",
     "bun", "wonder", "stage", "model", "rap", "sheep", "storm", "alps", "binary", "neat", "target", "meat", "you",
-    "unison", "passion", "ball", "showbiz", "game", "island", "cheer", "shy", "money"
+    "unison", "passion", "ball", "showbiz", "game", "island", "cheer", "test", "money"
 ]
 
-test = "thanksforplayingthepuzzlesolvingfestival"
+test = "youdiditcongratsyouvesolvedallthepuzzles"
 if len(test) != 40:
     print("hold up that test should be 40 chars")
     exit()
@@ -39,36 +39,202 @@ for c in test:
 
 print("")
 print("")
-print("=== POSSIBLE CLEARS ===")
+print("=== MAPPING ===")
 
 combos = {}
 order = []
 
 for k in neededCounts:
-    l = neededCounts[k]
-    while k not in combos or len(combos[k]) == 0:
-        combos[k] = [set(c) for c in combinations([a for a in answers if k in a], l)]
-        l -= 1
-    order.append((len(combos[k]), k))
+    for i in range(neededCounts[k]):
+        order.append((hasCounts[k] - neededCounts[k], k))
 order.sort()
-print(order)
 
-res = {}
-def rec(oi, pickedSet, leftovers, missesLeft):
-    global res, neededCounts, hasCounts
+def rec(oi, answersLeft, picks):
+    global test, order, neededCounts, hasCounts
     if oi >= len(order):
-        misses = "".join(sorted([k*leftovers[k] for k in leftovers]))
-        if misses not in res:
-            print(misses)
-            res[misses] = sum([hasCounts[c] for c in misses])
-        return
+        for c in test:
+            w = picks[c].pop()
+            print(c, w)
+        return True
 
-    for c in combos[order[oi][1]]:
-        fc = c.difference(pickedSet)
-        addedMisses = neededCounts[order[oi][1]] - len(fc)
-        if addedMisses <= missesLeft:
-            leftovers[order[oi][1]] = addedMisses
-            rec(oi + 1, pickedSet.union(fc), leftovers, missesLeft - addedMisses)
-            leftovers[order[oi][1]] = None
+    c = order[oi][1]
+    for a in answersLeft:
+        if c in a:
+            answersLeft.remove(a)
+            picks[c].append(a)
+            res = rec(oi + 1, answersLeft, picks)
+            if res: return True
+            answersLeft.add(a)
+            picks[c].pop()
+    return False
 
-rec(0, set(), {}, len([a for a in answers if a == ""]))
+rec(0, set(answers), {c:[] for c in set([k for k in neededCounts])})
+
+"""
+=== MAPPING ===
+t training
+h sheep
+a ramen
+n money
+k bakery
+s sister
+f fan
+o model
+r storm
+p piano
+l ball
+a target
+y you
+i rice
+n yarn
+g sewing
+t meat
+h shiny
+e muse
+p passion
+u unison
+z zura
+z showbiz
+l circle
+e cheer
+s stage
+o wonder
+l alps
+v dive
+i bibi
+n bun
+g game
+f ferry
+e test
+s island
+t neat
+i binary
+v clover
+a rap
+l angelic
+
+
+y ferry
+o wonder
+u muse
+s sister
+o passion
+l clover
+v dive
+e test
+d model
+a meat
+l circle
+l island
+t neat
+h cheer
+e angelic
+p piano
+u you
+z zura
+z showbiz
+l ball
+e sheep
+s sewing
+t training
+h shiny
+a target
+n bun
+k bakery
+y binary
+o storm
+u unison
+f fan
+o money
+r rice
+p rap
+l alps
+a game
+y yarn
+i bibi
+n ramen
+g stage
+
+y bakery
+o storm
+u bun
+d island
+i rice
+d model
+i dive
+t neat
+c cheer
+o you
+n ramen
+g training
+r ferry
+a rap
+t stage
+s sheep
+o money
+n binary
+s muse
+o wonder
+l angelic
+v clover
+i bibi
+n yarn
+g sewing
+a target
+l circle
+l alps
+o passion
+f fan
+t meat
+h shiny
+e test
+p piano
+u unison
+z showbiz
+z zura
+l ball
+e game
+s sister
+
+y binary
+o you
+u muse
+d model
+i training
+d island
+i bibi
+t sister
+c rice
+o storm
+n ramen
+g target
+r cheer
+a yarn
+t meat
+s stage
+y ferry
+o passion
+u unison
+v dive
+e sheep
+s sewing
+o piano
+l ball
+v clover
+e money
+d wonder
+a fan
+l angelic
+l circle
+t neat
+h shiny
+e game
+p rap
+u bun
+z zura
+z showbiz
+l alps
+e bakery
+s test
+
+"""
